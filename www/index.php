89b598573fb35a800f3b00cfd521fcf1447eed78
EC2 Default User <ec2-user@ip-172-31-28-8.us-west-2.compute.internal>
2017-05-16 05:31:33 +0000

    test commit

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Yuva DEMO Web Aplicationp";
+$AppName = "Yuva DEMO Web Application";
 
 ob_start();
 

74d366edf542daf9ef7e9c28e9e3c7309e4bacc7
root <root@ip-172-31-6-33.us-west-2.compute.internal>
2017-05-18 06:36:40 +0000

    venky tesgt commit

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Yuva Demo Web Application";
+$AppName = "Venkateswarlu Web Application";
 
 ob_start();
 
